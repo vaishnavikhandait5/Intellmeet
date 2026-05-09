6597a2b32d142684f8ce41db692a15d01521f0d0
student demo <vaishnavikhandait5@gmail.com>
2026-05-09 10:52:41 +0530

    Initial project upload

--- models/Meeting.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const meetingSchema = new mongoose.Schema({
-    title: String,
-    host: String,
-    participants: []
-});
-
-module.exports = mongoose.model("Meeting", meetingSchema);
